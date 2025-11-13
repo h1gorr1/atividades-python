860c82726b3f6e8224cabc148a26473e379fdacd
h1gorr1 <165603294+h1gorr1@users.noreply.github.com>
2025-11-13 17:29:06 -0300

    Add files via upload

---
+++ ex2.lista2.rafael.py
@@ -0,0 +1,22 @@
+# #2: Desenvolva um sistema de autenticação que permita ao usuário fazer login com
+# tentativas limitadas. O sistema deve ter diferentes níveis de usuário e implementar
+# bloqueio temporário após falhas consecutivas.
+# Requisitos técnicos:
+#  Use while para controlar o loop de tentativas
+#  Implemente try-except para tratamento de entrada
+#  Use if-elif-else para diferentes níveis de acesso
+#  Use range para controlar tempo de bloqueio
+#  Crie função para validação de credenciais 
+
+from time import sleep
+
+usuariodono = 1
+usuariogerente = 2
+usuariotrabalhador = 3 
+
+
+usuario = 
+senha = input("Digite Aqui A Senha!!! ")
+sleep(30)
+
+
